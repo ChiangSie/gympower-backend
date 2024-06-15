b900ed6c6d3271dc945eb75a3c3781c3048a27fb
ChiangSie <jimmy7369699@gmail.com>
2024-06-15 16:28:48 +0800

    後台vue

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import ViewUIPlus from 'view-ui-plus'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import 'view-ui-plus/dist/styles/viewuiplus.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(ViewUIPlus)
+app.mount('#app')
