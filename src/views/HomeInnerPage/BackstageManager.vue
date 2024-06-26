e83f87ef80308f02954c61cc843693a3b81919e0
ChiangSie <jimmy7369699@gmail.com>
2024-06-26 23:28:16 +0800

    test

--- src/views/HomeInnerPage/BackstageManager.vue
+++ src/views/HomeInnerPage/BackstageManager.vue
@@ -1,12 +1,132 @@
 <template>
   <section class="bsm">
     <h2>後台人員管理</h2>
+    <div class="bsmtop">
+      <button @click="modal2 = true" style="width: 120px; height: 30px">
+        <i class="fa-solid fa-plus"></i>新增
+      </button>
+      <!-- 燈箱 -->
+      <Modal
+        title="新增後台管理員"
+        v-model="modal2"
+        :mask-closable="false"
+        class-name="vertical-center-modal "
+      >
+        <div style="display: flex; flex-direction: column; align-items: center; gap: 3px">
+          <Space class="addInput" style="position: relative; left: 7.5px">
+            新增ID：<Input v-model="addAdminData.id" />
+          </Space>
+          <Space class="addInput"> 新增帳號：<Input v-model="addAdminData.acc" /> </Space>
+          <Space class="addInput"> 新增密碼：<Input v-model="addAdminData.psw" /> </Space>
+          <Space style="position: relative; right: 14px">
+            再次輸入密碼：<Input type="password" v-model="addAdminData.firstpsw" />
+          </Space>
+        </div>
+      </Modal>
+      <!-- 燈箱 -->
+      <Input
+        class="search-input"
+        search
+        enter-button
+        placeholder="搜尋"
+        v-model="search"
+        style="width: 300px"
+      />
+    </div>
     <hr />
+    <Table size="large" :columns="columns" :data="mangerdata" id="table01">
+      <template #am_id="{ row }">
+        <strong>{{ row.am_id }}</strong>
+      </template>
+      <template #am_acc="{ row }">
+        <span>{{ row.am_acc }}</span>
+      </template>
+      <template #am_psw="{ row }">
+        <span>{{ row.am_psw }}</span>
+      </template>
+      <template #am_status="{}">
+        <Switch true-color="#13ce66" false-color="#ff4949" :true-value="1" :false-value="0" />
+      </template>
+    </Table>
   </section>
 </template>
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      //<燈箱
+      modal2: false,
+      //<燈箱
+      //<新增管理員
+      addAdminData: {
+        id: '',
+        acc: '',
+        psw: '',
+        status: 1
+      },
+      //<新增管理員
+      //<第一次輸入密碼
+      firstpsw: '',
+      //<第一次輸入密碼
+      columns: [
+        {
+          title: '編號',
+          key: 'am_no',
+          align: 'center'
+        },
+        {
+          title: '管理ID',
+          key: 'am_id',
+          slot: 'am_id',
+          align: 'center'
+        },
+        {
+          title: '帳號',
+          key: 'am_acc',
+          slot: 'am_acc',
+          align: 'center'
+        },
+        {
+          title: '密碼',
+          key: 'am_psw',
+          slot: 'am_psw',
+          align: 'center'
+        },
+        {
+          title: '狀態',
+          key: 'am_status',
+          slot: 'am_status',
+          align: 'center'
+        }
+      ],
+      mangerdata: [
+        {
+          am_no: '1',
+          am_id: 'admin01',
+          am_acc: 'admin01',
+          am_psw: '123456',
+          am_status: '1'
+        },
+        {
+          am_no: '2',
+          am_id: 'admin02',
+          am_acc: 'admin02',
+          am_psw: '123456',
+          am_status: '1'
+        }
+      ]
+    }
+  },
+  methods: {}
+}
+</script>
 
 <style lang="scss" scoped>
+.vertical-center-modal {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 hr {
   width: 100%;
   border-width: 1px;
@@ -16,6 +136,14 @@ hr {
 .bsm {
   width: 100%;
   padding: 20px;
+  .bsmtop {
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+    button {
+      margin: 10px 0;
+    }
+  }
   h2 {
     font-size: 2vw;
   }
--- src/views/HomeInnerPage/CoachView.vue
+++ src/views/HomeInnerPage/CoachView.vue
@@ -1,3 +1,113 @@
 <template>
-  <div>iii</div>
+  <section>
+    <h2>教練管理</h2>
+    <div class="memtop">
+      <Input class="search-input" search enter-button placeholder="搜尋" style="width: 300px" />
+    </div>
+    <hr />
+    <Table size="large" :columns="columns" :data="memdata">
+      <template #coach_id="{ row }">
+        <strong>{{ row.coach_id }}</strong>
+      </template>
+      <template #coach_name="{ row }">
+        <strong>{{ row.coach_name }}</strong>
+      </template>
+      <template #coach_img="{ row }">
+        <strong>{{ row.coach_img }}</strong>
+      </template>
+      <template #coach_licc="{ row }">
+        <strong>{{ row.coach_licc }}</strong>
+      </template>
+      <template #coach_rcm="{}">
+        <Switch true-color="#13ce66" false-color="#ff4949" :true-value="1" :false-value="0" />
+      </template>
+      <template #coach_info="{ row }">
+        <strong>{{ row.coach_info }}</strong>
+      </template>
+    </Table>
+  </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      columns: [
+        {
+          title: '教練編號',
+          key: 'coach_id',
+          align: 'center'
+        },
+        {
+          title: '教練姓名',
+          key: 'coach_name',
+          slot: 'coach_name',
+          align: 'center'
+        },
+        {
+          title: '教練照片',
+          key: 'coach_img',
+          slot: 'coach_img',
+          align: 'center'
+        },
+        {
+          title: '專業證照',
+          key: 'coach_licc',
+          slot: 'coach_licc',
+          align: 'center'
+        },
+        {
+          title: '推薦教練',
+          key: 'coach_rcm',
+          slot: 'coach_rcm',
+          align: 'center'
+        },
+        {
+          title: '教練簡介',
+          key: 'coach_info',
+          slot: 'coach_info',
+          align: 'center'
+        }
+      ],
+      memdata: [
+        {
+          coach_id: '1',
+          coach_name: 'wakawaka',
+          coach_img: '',
+          coach_licc: '',
+          coach_rcm: '1',
+          coach_info: 'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww'
+        },
+        {
+          coach_id: '2',
+          coach_name: 'galagala',
+          coach_img: '',
+          coach_licc: '',
+          coach_rcm: '1',
+          coach_info: 'BBbbbbbbBBBBbbBBBbBBBBB'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+hr {
+  width: 100%;
+  border-width: 1px;
+  border-color: #000000;
+  margin: 10px 0;
+}
+section {
+  width: 100%;
+  padding: 20px;
+  h2 {
+    font-size: 2vw;
+  }
+  .coachtop {
+    display: flex;
+    justify-content: end;
+  }
+}
+</style>
